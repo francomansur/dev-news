0f9b11c586d5d1cb68adef5806366c362fe3561b
Franco Mansur <francomansur2000@gmail.com>
2025-12-07 13:09:19 -0300

    Mensagem de em contrução

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Bem vindo ao DevNews!</h1>;
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🚧 Em construção 🚧</h1>
+      <p>Volte em breve!</p>
+    </div>
+  );
 }
 
 export default Home;
