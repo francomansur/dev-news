06f49751d0f1ffdadf724be0054495a742592ba2
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-12-24 19:17:07 -0300

    ajustes de SSL

--- infra/database.js
+++ infra/database.js
@@ -1,5 +1,14 @@
 import { Client } from "pg";
 
+function getSSLValues() {
+  if (process.env.PGSSLMODE === "require") {
+    return {
+      rejectUnauthorized: true,
+    };
+  }
+  return false;
+}
+
 async function query(queryObject) {
   const client = new Client({
     user: process.env.POSTGRES_USER,
@@ -7,14 +16,7 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     host: process.env.POSTGRES_HOST,
     password: process.env.POSTGRES_PASSWORD,
-  });
-
-  console.log("Credenciais Postgres:", {
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    port: process.env.POSTGRES_PORT,
-    host: process.env.POSTGRES_HOST,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
   });
 
   try {
@@ -23,7 +25,7 @@ async function query(queryObject) {
     return result;
   } catch (err) {
     console.error(err);
-    throw error;
+    throw err;
   } finally {
     await client.end();
   }
