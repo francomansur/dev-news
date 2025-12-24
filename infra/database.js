80a3d0eea58eb2d03084dc294061200773593fae
Franco Mansur Giani <160537805+francomansur@users.noreply.github.com>
2025-12-24 19:23:10 -0300

    ajustes de SSL, retornando true em producao

--- infra/database.js
+++ infra/database.js
@@ -6,7 +6,8 @@ function getSSLValues() {
       rejectUnauthorized: true,
     };
   }
-  return false;
+
+  return process.env.NODE_ENV === "production" ? true : false;
 }
 
 async function query(queryObject) {
